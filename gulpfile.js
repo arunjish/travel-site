8f203437ab8cd1299120278c3a351eb8a8f4155c
Arun jishnu <lettertojishnu@gmail.com>
2017-08-07 13:05:56 +0530

    auto refresh and css inject

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,13 @@ postcss = require('gulp-postcss')
 autoprefixer = require('autoprefixer');
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
-
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default',function() {
   console.log("hiiiiii");
 });
+
 gulp.task('html',function() {
 
   console.log('something changed on html!!!!!');
@@ -18,20 +19,31 @@ gulp.task('html',function() {
 
 gulp.task('styles',function() {
 
-return gulp.src('./app/assets/styles/styles.css')
-.pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
-.pipe(gulp.dest('./app/temp/styles'));
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
 
 });
 
 gulp.task('watch',function() {
 
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir : "app"
+    }
+  });
   watch('./app/index.html', function () {
-    gulp.start('html');
+    browserSync.reload();
   });
 
   watch('./app/assets/styles/**/*.css',function () {
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 
+});
+
+gulp.task('cssInject',['styles'],function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
