0942ee0080cc04f03b3fb313c7289ad6d1c667ec
Arun <lettertojishnu@gmail.com>
2017-08-13 23:53:20 +0530

    java script organization webkit babel

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,19 @@
-console.log("this is text");
+var $ = require('jquery');
+import Person from './modules/Person';
+
+class Adult extends Person {
+  payTaxes() {
+    console.log(this.name + " now owes $0 in taxes.");
+  }
+}
+
+alert("ABC 321");
+
+var john = new Person("John Doe", "blue");
+john.greet();
+
+var jane = new Adult("Jane Smith", "orange");
+jane.greet();
+jane.payTaxes();
+
+$("h1").remove();
